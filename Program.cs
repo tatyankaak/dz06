dd34710a18178b7886d84a2814599cafae35778a
Татьяна <robsten9086@mail.ru>
2023-07-09 16:20:19 +0300

    задача 6

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+					
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        
+        if(number % 2 == 0)
+            Console.WriteLine("да.");
+        else
+            Console.WriteLine("нет");
+    }
+}
